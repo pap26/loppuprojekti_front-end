2cd93adc2617d03aed61a7449b4a38f81ca233fd
svakel <sari.vakela@gmail.com>
2018-11-23 12:04:09 +0200

    datahaku-korjaus

--- src/components/TapahtumaSivu.js
+++ src/components/TapahtumaSivu.js
@@ -3,43 +3,63 @@ import Tapahtuma from './Tapahtuma.js';
 import Instagram from './Instagram.js';
 import Navigation from './Navigation.js';
 
-let tapahtumadata = [
-    {id:1, 
-    summary: "matsi : Harjoituspeli KOPSE valkoinen - FC Viikingit", 
-    //tapahtuman_nimi: "Harjoituspeli KOPSE valkoinen - FC Viikingit", 
-    start: {
-        dateTime: {
-        value: 1542812400000,
-        dateOnly: false,
-        timeZoneShift: 120
-        },
-        timeZone: "Europe/Helsinki"
-        },
-    end: {
-        dateTime: {
-        value: 1542816000000,
-        dateOnly: false,
-        timeZoneShift: 120
-        },
-        timeZone: "Europe/Helsinki"
-        },
+// let tapahtumadata = [
+//     {id:1, 
+//     summary: "matsi : Harjoituspeli KOPSE valkoinen - FC Viikingit", 
+//     //tapahtuman_nimi: "Harjoituspeli KOPSE valkoinen - FC Viikingit", 
+//     start: {
+//         dateTime: {
+//         value: 1542812400000,
+//         dateOnly: false,
+//         timeZoneShift: 120
+//         },
+//         timeZone: "Europe/Helsinki"
+//         },
+//     end: {
+//         dateTime: {
+//         value: 1542816000000,
+//         dateOnly: false,
+//         timeZoneShift: 120
+//         },
+//         timeZone: "Europe/Helsinki"
+//         },
 
-    dateTime: {
-        value: 1542812400000,
-        timeZoneShift: 120
-    },
-    location: "Havukoski TN, Koivukylänväylä 32, 01360 Vantaa",
-    in: true
-    }];
+//     dateTime: {
+//         value: 1542812400000,
+//         timeZoneShift: 120
+//     },
+//     location: "Havukoski TN, Koivukylänväylä 32, 01360 Vantaa",
+//     in: true
+//     }];
 
 class TapahtumaSivu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tapahtumat: []
+        }
+
+    }
+    componentDidMount() {
+        this.haeKymmenen();
+    }
+    haeKymmenen = () => {
+    fetch('api/kalenteri/kymmenen') 
+        .then(response=> response.json())
+        // yllä ole ennemmin response.json, koska usein haetaan jsonia
+        .then(data=> {
+            // jos haet esim jsonista kysysmys-tietoa, käytä json.kysymys
+            this.setState({tapahtumat: data})
+            });
+    };        
+
 
     render() {
         return (
             <div>
                 <Navigation />
                 <div className="tapahtuma_bg">
-                <Tapahtuma lista={tapahtumadata}/>
+                <Tapahtuma lista={this.state.tapahtumat}/>
                 </div>
                 <div className="instagam">
                 <Instagram />
